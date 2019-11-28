904529b13279f31651837bca05783ec954619f4b
Alok Tripathy <kullalok@gmail.com>
2019-11-27 16:58:50 -0800

    Runs without errors

--- slurm_pyg_tut.sh
+++ slurm_pyg_tut.sh
@@ -22,14 +22,14 @@
 
 ## Run command
 module load python/3.6
-module load ml/torch/torch7
 module unload gcc/4.8.5
-module load gcc/6.3.0
+module load gcc/5.4.0
 module unload cuda/8.0
-module load cuda/10.0
-source virtual3/bin/activate
+module load cuda/9.1
+# source virtual3/bin/activate
+source gnn/bin/activate
 
 nvidia-smi
 
-# python tut-recsys.py
-python pytorch_geometric/examples/gcn.py
+python tut-recsys.py
+# python pytorch_geometric/examples/gcn.py
--- tut-recsys.py
+++ tut-recsys.py
@@ -1,40 +1,131 @@
-import torch
-import pandas as pd
 import numpy as np
-
-from torch_geometric.data import InMemoryDataset
-from tqdm import tqdm
-from sklearn.preprocessing import LabelEncoder
-
-from torch.nn import Sequential as Seq, Linear, ReLU
-from torch_geometric.nn import MessagePassing
-from torch_geometric.utils import remove_self_loops, add_self_loops
-
-from torch_geometric.nn import TopKPooling
-from torch_geometric.nn import global_mean_pool as gap, global_max_pool as gmp
-
-from torch.utils.data import DataLoader
-
+import pandas as pd
+import pickle
+import csv
+import os
+import torch
 import torch.nn.functional as F
 
-embed_dim = 128
-num_epochs = 1
+from torch.nn import Sequential as Seq, Linear, ReLU
+
+from torch_geometric.data import Data, InMemoryDataset, DataLoader
+from torch_geometric.nn import MessagePassing, GraphConv, TopKPooling, GatedGraphConv
+from torch_geometric.nn import global_mean_pool as gap, global_max_pool as gmp
+from torch_geometric.utils import remove_self_loops, add_self_loops
+
+from tqdm import tqdm
+
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import roc_auc_score
+
+np.random.seed(42)
+
+df = pd.read_csv('./yoochoose-dats/yoochoose-clicks.dat', header=None)
+df.columns=['session_id', 'timestamp', 'item_id', 'category']
+
+buy_df = pd.read_csv('./yoochoose-dats/yoochoose-buys.dat', header=None)
+buy_df.columns=['session_id', 'timestamp', 'item_id', 'price', 'quantity']
+buy_df.head(20)
+
+print(df.nunique())
+print(buy_df.nunique())
+
+df['valid_session'] = df.session_id.map(df.groupby('session_id')['item_id'].size() > 2)
+df = df.loc[df.valid_session].drop('valid_session', axis=1)
+print(df.nunique())
+
+sampled_session_id = np.random.choice(df.session_id.unique(), 1000000, replace=False)
+df = df.loc[df.session_id.isin(sampled_session_id)]
+print(df.nunique())
+
+print(df.isna().sum())
+print(df.groupby('session_id')['item_id'].size().mean())
+
+item_encoder = LabelEncoder()
+df['item_id'] = item_encoder.fit_transform(df.item_id)
+df.head()
+
+df['label'] = df.session_id.isin(buy_df.session_id)
+df.head()
+
+print(df.drop_duplicates('session_id')['label'].mean())
+
+class YooChooseBinaryDataset(InMemoryDataset):
+    def __init__(self, root, transform=None, pre_transform=None):
+        super(YooChooseBinaryDataset, self).__init__(root, transform, pre_transform)
+        self.data, self.slices = torch.load(self.processed_paths[0])
+
+    @property
+    def raw_file_names(self):
+        return []
+    @property
+    def processed_file_names(self):
+        return ['./yoochoose-dats/yoochoose_click_binary_1M_sess.dataset']
+
+    def download(self):
+        pass
+    
+    def process(self):
+        
+        data_list = []
+
+        # process by session_id
+        grouped = df.groupby('session_id')
+        for session_id, group in tqdm(grouped):
+            sess_item_id = LabelEncoder().fit_transform(group.item_id)
+            group = group.reset_index(drop=True)
+            group['sess_item_id'] = sess_item_id
+            node_features = group.loc[group.session_id==session_id,['sess_item_id','item_id']].sort_values('sess_item_id').item_id.drop_duplicates().values
+
+            node_features = torch.LongTensor(node_features).unsqueeze(1)
+            target_nodes = group.sess_item_id.values[1:]
+            source_nodes = group.sess_item_id.values[:-1]
+
+            edge_index = torch.tensor([source_nodes,
+                                   target_nodes], dtype=torch.long)
+            x = node_features
+
+            y = torch.FloatTensor([group.label.values[0]])
+
+            data = Data(x=x, edge_index=edge_index, y=y)
+            data_list.append(data)
+        
+        data, slices = self.collate(data_list)
+        torch.save((data, slices), self.processed_paths[0])
+
+dataset = YooChooseBinaryDataset(root='./')
+
+dataset = dataset.shuffle()
+train_dataset = dataset[:800000]
+val_dataset = dataset[800000:900000]
+test_dataset = dataset[900000:]
+print(len(train_dataset), len(val_dataset), len(test_dataset))
+
+batch_size = 1024
+train_loader = DataLoader(train_dataset, batch_size=batch_size)
+val_loader = DataLoader(val_dataset, batch_size=batch_size)
+test_loader = DataLoader(test_dataset, batch_size=batch_size)
+
+num_items = df.item_id.max() + 1
+print(num_items)
 
 class SAGEConv(MessagePassing):
     def __init__(self, in_channels, out_channels):
-        super(SAGEConv, self).__init__(aggr='max')
+        super(SAGEConv, self).__init__(aggr='max') #  "Max" aggregation.
         self.lin = torch.nn.Linear(in_channels, out_channels)
         self.act = torch.nn.ReLU()
-        self.update_lin = torch.nn.Linear(in_channels + out_channels, out_channels, bias=False)
+        self.update_lin = torch.nn.Linear(in_channels + out_channels, in_channels, bias=False)
         self.update_act = torch.nn.ReLU()
-
+        
     def forward(self, x, edge_index):
         # x has shape [N, in_channels]
-        # edge_index has shape [2, E] (edge list I guess?)
-
+        # edge_index has shape [2, E]
+        
+        
         edge_index, _ = remove_self_loops(edge_index)
         edge_index, _ = add_self_loops(edge_index, num_nodes=x.size(0))
-
+        
+        
         return self.propagate(edge_index, size=(x.size(0), x.size(0)), x=x)
 
     def message(self, x_j):
@@ -42,18 +133,21 @@ class SAGEConv(MessagePassing):
 
         x_j = self.lin(x_j)
         x_j = self.act(x_j)
-
+        
         return x_j
 
     def update(self, aggr_out, x):
         # aggr_out has shape [N, out_channels]
 
-        new_embedding = torch.cat([aggr_out, x], dim=1)
 
+        new_embedding = torch.cat([aggr_out, x], dim=1)
+        
         new_embedding = self.update_lin(new_embedding)
         new_embedding = self.update_act(new_embedding)
-
+        
         return new_embedding
+
+embed_dim = 128
 
 class Net(torch.nn.Module):
     def __init__(self):
@@ -61,43 +155,37 @@ class Net(torch.nn.Module):
 
         self.conv1 = SAGEConv(embed_dim, 128)
         self.pool1 = TopKPooling(128, ratio=0.8)
-
         self.conv2 = SAGEConv(128, 128)
         self.pool2 = TopKPooling(128, ratio=0.8)
-
         self.conv3 = SAGEConv(128, 128)
         self.pool3 = TopKPooling(128, ratio=0.8)
-
-        self.item_embedding = torch.nn.Embedding(num_embeddings=df.item_id.max() + 1, embedding_dim=embed_dim)
-
+        self.item_embedding = torch.nn.Embedding(num_embeddings=df.item_id.max() +1, embedding_dim=embed_dim)
         self.lin1 = torch.nn.Linear(256, 128)
         self.lin2 = torch.nn.Linear(128, 64)
         self.lin3 = torch.nn.Linear(64, 1)
-
         self.bn1 = torch.nn.BatchNorm1d(128)
         self.bn2 = torch.nn.BatchNorm1d(64)
-
         self.act1 = torch.nn.ReLU()
-        self.act2 = torch.nn.ReLU()
-
+        self.act2 = torch.nn.ReLU()        
+  
     def forward(self, data):
         x, edge_index, batch = data.x, data.edge_index, data.batch
         x = self.item_embedding(x)
-        x = x.squeeze(1)
+        x = x.squeeze(1)        
 
         x = F.relu(self.conv1(x, edge_index))
 
-        x, edge_index, _, batch = self.pool1(x, edge_index, None, batch)
+        x, edge_index, _, batch, _ = self.pool1(x, edge_index, None, batch)
         x1 = torch.cat([gmp(x, batch), gap(x, batch)], dim=1)
 
         x = F.relu(self.conv2(x, edge_index))
-
-        x, edge_index, _, batch = self.pool2(x, edge_index, None, batch)
+     
+        x, edge_index, _, batch, _ = self.pool2(x, edge_index, None, batch)
         x2 = torch.cat([gmp(x, batch), gap(x, batch)], dim=1)
 
         x = F.relu(self.conv3(x, edge_index))
 
-        x, edge_index, _, batch = self.pool3(x, edge_index, None, batch)
+        x, edge_index, _, batch, _ = self.pool3(x, edge_index, None, batch)
         x3 = torch.cat([gmp(x, batch), gap(x, batch)], dim=1)
 
         x = x1 + x2 + x3
@@ -105,97 +193,60 @@ class Net(torch.nn.Module):
         x = self.lin1(x)
         x = self.act1(x)
         x = self.lin2(x)
-        x = self.act2(x)
-
-        x = F.dropout(x, p=0.5, training=self.trainign)
+        x = self.act2(x)      
+        x = F.dropout(x, p=0.5, training=self.training)
 
         x = torch.sigmoid(self.lin3(x)).squeeze(1)
 
         return x
 
-        
-
-class YooChooseBinaryDataset(InMemoryDataset):
-    def __init__(self, root, transform=None, pre_transform=None):
-        super(YooChooseBinaryDataset, self).__init__(root, transform, pre_Transform)
-        self.data, self.slices = torch.load(self.processed_paths[0])
-
-    @property
-    def raw_file_names(self):
-        return []
-
-    @property
-    def processed_File_names(self):
-        return ['yoochoose-dats/yoochoose_click_binary_1M_sess.dataset']
-
-    def download(self):
-        pass
-        
-    def process(self):
-        data_list = []
-
-        # process by session id
-        grouped = df.groupby('session_id')
-        for sesion_id, group in tqdm(grouped):
-            sess_item_id = LabelEncoder
-            sess_item_id = LabelEncoder().fit_transform(group.item_id)
-            group = group.reset_index(drop=True)
-            group['sess_item_id'] = sess_item_id
-            node_features = group.loc[group.session_id==session_id,['sess_item_id','item_id']].sort_values(
-                                            'sess_item_id').item_id.drop_duplicates().values
-
-            node_features = torch.LongTensor(node_features).unsqueeze(1)
-            target_nodes = group.sess_item_id.values[1:]
-            source_nodes = group.sess_item_id.values[:-1]
-
-            edge_index = torch.tensor([source_nodes, target_nodes], dtype=torch.long)
-            x = node_features
-
-            y = torch.FloatTensor([group.label.values[0]])
-
-            data = Data(x=x, edge_index=edge_index, y=y)
-            data_list.append(data) 
-        
-        data, slices = self.collate(data_list)
-        torch.save((data, slices), self.processed_paths[0]) 
-
+device = torch.device('cuda')
+model = Net().to(device)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.005)
+crit = torch.nn.BCELoss()
 
 def train():
     model.train()
 
     loss_all = 0
-    for dat in train_loader:
+    iter_count = 0
+    for data in train_loader:
         data = data.to(device)
         optimizer.zero_grad()
+        print(data)
         output = model(data)
         label = data.y.to(device)
         loss = crit(output, label)
         loss.backward()
         loss_all += data.num_graphs * loss.item()
         optimizer.step()
-
     return loss_all / len(train_dataset)
 
-df = pd.read_csv('yoochoose-dats/yoochoose-clicks.dat', header=None)
-df.columns=['session_id', 'timestamp', 'item_id', 'category']
+def evaluate(loader):
+    model.eval()
 
-buy_df = pd.read_csv('yoochoose-dats/yoochoose-buys.dat', header=None)
-buy_df.columns=['session_id', 'timestamp', 'item_id', 'price', 'quantity']
+    predictions = []
+    labels = []
 
-item_encoder = LabelEncoder()
-df['item_id'] = item_encoder.fit_transform(df.item_id)
-df.head()
+    with torch.no_grad():
+        for data in loader:
 
-# randomly sample set of sessions
-sampled_session_id = np.random.choice(df.session_id.unique(), 1000000, replace=False)
-df = df.loc[df.session_id.isin(sampled_session_id)]
-df.nunique()
+            data = data.to(device)
+            pred = model(data).detach().cpu().numpy()
 
-device = torch.device('cuda')
-model = Net().to(device)
-optimizer = torch.optim.Adam(model.parameters(), lr=0.005)
-crit = torch.nn.BCELoss()
-train_loader = DataLoader(train_dataset, batch_size=batch_size)
-for epoch in range(num_epochs):
-    train()
+            label = data.y.detach().cpu().numpy()
+            predictions.append(pred)
+            labels.append(label)
 
+    predictions = np.hstack(predictions)
+    labels = np.hstack(labels)
+    
+    return roc_auc_score(labels, predictions)
+
+for epoch in range(1):
+    loss = train()
+    train_acc = evaluate(train_loader)
+    val_acc = evaluate(val_loader)    
+    test_acc = evaluate(test_loader)
+    print('Epoch: {:03d}, Loss: {:.5f}, Train Auc: {:.5f}, Val Auc: {:.5f}, Test Auc: {:.5f}'.
+          format(epoch, loss, train_acc, val_acc, test_acc))
