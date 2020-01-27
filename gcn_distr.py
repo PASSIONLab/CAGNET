24eb7b32957eb8332afcfbf9e5eadf097333c553
Alok Tripathy <kullalok@gmail.com>
2020-01-27 15:47:27 -0800

    Beginning distributed computation

--- gcn_distr.py
+++ gcn_distr.py
@@ -1,36 +1,21 @@
+import os
 import os.path as osp
 import argparse
 
 import torch
-from torch.nn import Parameter
-import torch.nn.functional as F
+import torch.distributed as dist
 
 from torch_geometric.datasets import Planetoid
 from torch_geometric.nn import GCNConv, ChebConv  # noqa
-import torch_geometric.transforms as T
 from torch_geometric.utils import add_self_loops, add_remaining_self_loops, degree, to_dense_adj
+import torch_geometric.transforms as T
+
+from torch.multiprocessing import Process
+
+from torch.nn import Parameter
+import torch.nn.functional as F
 
 from torch_scatter import scatter_add
-
-parser = argparse.ArgumentParser()
-parser.add_argument('--use_gdc', action='store_true',
-                    help='Use GDC preprocessing.')
-args = parser.parse_args()
-
-dataset = 'Cora'
-path = osp.join(osp.dirname(osp.realpath(__file__)), '..', 'data', dataset)
-dataset = Planetoid(path, dataset, T.NormalizeFeatures())
-data = dataset[0]
-
-seed = 0
-
-if args.use_gdc:
-    gdc = T.GDC(self_loop_weight=1, normalization_in='sym',
-                normalization_out='col',
-                diffusion_kwargs=dict(method='ppr', alpha=0.05),
-                sparsification_kwargs=dict(method='topk', k=128,
-                                           dim=0), exact=True)
-    data = gdc(data)
 
 def norm(edge_index, num_nodes, edge_weight=None, improved=False,
          dtype=None):
@@ -72,31 +57,6 @@ class GCNFunc(torch.autograd.Function):
         grad_weight = torch.mm(torch.mm(inputs.t(), adj_matrix), grad_output)
         return grad_input, grad_weight, None, None
 
-# device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-device = torch.device('cuda')
-
-torch.manual_seed(seed)
-weight1_nonleaf = torch.rand(dataset.num_features, 16, requires_grad=True)
-weight1_nonleaf = weight1_nonleaf.to(device)
-weight1_nonleaf.retain_grad()
-
-weight2_nonleaf = torch.rand(16, dataset.num_classes, requires_grad=True)
-weight2_nonleaf = weight2_nonleaf.to(device)
-weight2_nonleaf.retain_grad()
-
-# model, data = Net().to(device), data.to(device)
-data = data.to(device)
-data.x.requires_grad = True
-inputs = data.x.to(device)
-
-edge_index = data.edge_index
-adj_matrix = to_dense_adj(edge_index)[0].to(device)
-
-weight1 = Parameter(weight1_nonleaf)
-weight2 = Parameter(weight2_nonleaf)
-# optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
-optimizer = torch.optim.Adam([weight1, weight2], lr=0.01)
-
 def train():
     outputs = GCNFunc.apply(inputs, weight1, adj_matrix)
     outputs = GCNFunc.apply(outputs, weight2, adj_matrix)
@@ -115,14 +75,86 @@ def test(outputs):
     return accs
 
 
-best_val_acc = test_acc = 0
-outputs = None
-for epoch in range(1, 201):
-# for epoch in range(1):
-    outputs = train()
-    train_acc, val_acc, tmp_test_acc = test(outputs)
-    if val_acc > best_val_acc:
-        best_val_acc = val_acc
-        test_acc = tmp_test_acc
-    log = 'Epoch: {:03d}, Train: {:.4f}, Val: {:.4f}, Test: {:.4f}'
-    print(log.format(epoch, train_acc, best_val_acc, test_acc))
+def run(rank, size):
+    best_val_acc = test_acc = 0
+    outputs = None
+    for epoch in range(1, 201):
+    # for epoch in range(1):
+        outputs = train()
+        train_acc, val_acc, tmp_test_acc = test(outputs)
+        if val_acc > best_val_acc:
+            best_val_acc = val_acc
+            test_acc = tmp_test_acc
+        log = 'Epoch: {:03d}, Train: {:.4f}, Val: {:.4f}, Test: {:.4f}'
+        print(log.format(epoch, train_acc, best_val_acc, test_acc))
+
+def init_process(rank, rsize, fn, backend='mpi'):
+    os.environ['MASTER_ADDR'] = '127.0.0.1'
+    os.environ['MASTER_PORT'] = '29500'
+    dist.init_process_group(backend, rank=rank, world_size=size)
+    fn(rank, size)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--use_gdc', action='store_true',
+                        help='Use GDC preprocessing.')
+    parser.add_argument('--processes', metavar='P', type=int,
+                        help='Number of processes')
+    args = parser.parse_args()
+    print(args)
+    P = args.processes
+    if P is None:
+        P = 1
+    
+    print("Processes: " + str(P))
+
+    dataset = 'Cora'
+    path = osp.join(osp.dirname(osp.realpath(__file__)), '..', 'data', dataset)
+    dataset = Planetoid(path, dataset, T.NormalizeFeatures())
+    data = dataset[0]
+
+    seed = 0
+
+    if args.use_gdc:
+        gdc = T.GDC(self_loop_weight=1, normalization_in='sym',
+                    normalization_out='col',
+                    diffusion_kwargs=dict(method='ppr', alpha=0.05),
+                    sparsification_kwargs=dict(method='topk', k=128,
+                                               dim=0), exact=True)
+        data = gdc(data)
+
+    # device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    # device = torch.device('cuda')
+    device = torch.device('cuda')
+
+    torch.manual_seed(seed)
+    weight1_nonleaf = torch.rand(dataset.num_features, 16, requires_grad=True)
+    weight1_nonleaf = weight1_nonleaf.to(device)
+    weight1_nonleaf.retain_grad()
+
+    weight2_nonleaf = torch.rand(16, dataset.num_classes, requires_grad=True)
+    weight2_nonleaf = weight2_nonleaf.to(device)
+    weight2_nonleaf.retain_grad()
+
+    # model, data = Net().to(device), data.to(device)
+    data = data.to(device)
+    data.x.requires_grad = True
+    inputs = data.x.to(device)
+
+    edge_index = data.edge_index
+    adj_matrix = to_dense_adj(edge_index)[0].to(device)
+
+    weight1 = Parameter(weight1_nonleaf)
+    weight2 = Parameter(weight2_nonleaf)
+    # optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+    optimizer = torch.optim.Adam([weight1, weight2], lr=0.01)
+
+    size = 2
+    processes = []
+    for rank in range(size):
+        p = Process(target=init_process, args=(rank, size, run))
+        p.start()
+        processes.append(p)
+
+    for p in processes:
+        p.join()
