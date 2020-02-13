0e8a78c6a6db6b95c3642f74ec5436d9f6d335fa
Alok Tripathy <kullalok@gmail.com>
2020-02-12 16:41:13 -0800

    Print outputs in gcn.py

--- gcn.py
+++ gcn.py
@@ -89,15 +89,19 @@ def test(outputs):
     return accs
 
 
-best_val_acc = test_acc = 0
-outputs = None
-# for epoch in range(1, 201):
-for epoch in range(1):
-    outputs = train()
-    train_acc, val_acc, tmp_test_acc = test(outputs)
-    if val_acc > best_val_acc:
-        best_val_acc = val_acc
-        test_acc = tmp_test_acc
-    log = 'Epoch: {:03d}, Train: {:.4f}, Val: {:.4f}, Test: {:.4f}'
-    print(log.format(epoch, train_acc, best_val_acc, test_acc))
-# print(outputs)
+def main(): 
+    best_val_acc = test_acc = 0
+    outputs = None
+    # for epoch in range(1, 201):
+    for epoch in range(1):
+        outputs = train()
+        train_acc, val_acc, tmp_test_acc = test(outputs)
+        if val_acc > best_val_acc:
+            best_val_acc = val_acc
+            test_acc = tmp_test_acc
+        log = 'Epoch: {:03d}, Train: {:.4f}, Val: {:.4f}, Test: {:.4f}'
+        print(log.format(epoch, train_acc, best_val_acc, test_acc))
+    return outputs
+
+if __name__=='__main__':
+    print(main())
